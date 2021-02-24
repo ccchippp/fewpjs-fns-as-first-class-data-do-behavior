8ac3015d28e35ad49c976811c810bcdfaa0dd349
ccchippp <clempke90@gmail.com>
2021-02-23 20:15:05 -0500

    commit tree 6677aa6e2de63998b000a986eb1782f16101941a

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = time.split(":",1)
+  let parsed = parseInt(hour)
+  if (parsed < 12) { return "Good Morning"}
+  else if (parsed > 17) { return "Good Evening"}
+  else return "Good Afternoon"
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById("greeting").innerText = str
+}
